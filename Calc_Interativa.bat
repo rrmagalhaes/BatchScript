f13e47b6ca091e8bf309a3c69ccb1c89eb590db6
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2020-07-18 17:32:15 -0300

    Alterado defin. de operador e incluso bloco soma.

--- Calc_Interativa.bat
+++ Calc_Interativa.bat
@@ -2,6 +2,8 @@
 
 :: Apresentação e escolha do tipo de cálculo
 
+:inicio
+
 echo Vai ser bem legal fazer uma conta juntos!
 echo Para iniciar precisamos definir qual vai ser nosso calculo!
 echo Decida que tipo de conta quer fazer digitando o numero referente:
@@ -9,18 +11,24 @@ echo 1 - SOMA
 echo 2 - SUBTRACAO
 echo 3 - MULTIPLICACAO
 echo 4 - DIVISAO
+echo 5 - SAIR SEM FAZER CONTA
 
 echo.
-
-set /p "calc=Digite um numero de 1 a 4: "
+:: Deinifição do operador
+choice /c "12345" /n /m 
+goto %errorlevel%
 
 cls
 
-echo Show, ja temos o tipo de calculo, agora precisamos dos numeros que vamos calcular.
+:: Soma
+:1
+echo Otima opcao, vamos colocar os numeros que vamos somar.
 echo.
 set /p "n1=Digite o primeiro numero: "
 echo.
 set /p "n2=Massa, agora digite o segundo numero para finalizarmos o calculo: "
+set /a soma=%n1%+%n2%
 
-
+echo A soma de %n1% + %n2% = %soma%
+ 
 pause >nul
