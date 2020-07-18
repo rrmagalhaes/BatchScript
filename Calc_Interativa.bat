1f9b8790508f0d97c6ac5e32ddf7e0468d3b88c8
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2020-07-18 15:14:04 -0300

    Colocada as variáveis calc, n1 e n2 para calcular

--- Calc_Interativa.bat
+++ Calc_Interativa.bat
@@ -1,11 +1,26 @@
 @echo off
 
+:: Apresentação e escolha do tipo de cálculo
+
 echo Vai ser bem legal fazer uma conta juntos!
-echo Pra começar precisamos definir o que será nosso calculo!
-echo Decida que tipo de conta quer fazer:
+echo Para iniciar precisamos definir qual vai ser nosso calculo!
+echo Decida que tipo de conta quer fazer digitando o numero referente:
 echo 1 - SOMA
 echo 2 - SUBTRACAO
 echo 3 - MULTIPLICACAO
 echo 4 - DIVISAO
+
+echo.
+
+set /p "calc=Digite um numero de 1 a 4: "
+
+cls
+
+echo Show, ja temos o tipo de calculo, agora precisamos dos numeros que vamos calcular.
+echo.
+set /p "n1=Digite o primeiro numero: "
+echo.
+set /p "n2=Massa, agora digite o segundo numero para finalizarmos o calculo: "
+
 
 pause >nul
