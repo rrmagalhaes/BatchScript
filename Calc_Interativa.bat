92f3e614195245a12def69a965bee836e6a8830e
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2020-07-18 15:01:43 -0300

    Mostrando as escolhas de qual será o cálculo.

---
+++ Calc_Interativa.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Vai ser bem legal fazer uma conta juntos!
+echo Pra começar precisamos definir o que será nosso calculo!
+echo Decida que tipo de conta quer fazer:
+echo 1 - SOMA
+echo 2 - SUBTRACAO
+echo 3 - MULTIPLICACAO
+echo 4 - DIVISAO
+
+pause >nul
