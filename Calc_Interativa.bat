34d8738cb7669600c5144bd6c3f3bd7eab641cb9
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2020-07-22 17:59:22 -0300

    Correção de texto no .bat

--- Calc_Interativa.bat
+++ Calc_Interativa.bat
@@ -1,95 +1,94 @@
 @echo off
 
-:: Apresentaï¿½ï¿½o e escolha do tipo de cï¿½lculo
+:: Apresenta‡Æo e escolha do tipo de c lculo
 
 :inicio
 cls
 
 echo Vai ser bem legal fazer uma conta juntos!
-echo Para iniciar precisamos definir qual vai ser nosso cï¿½lculo!
-echo Decida que tipo de conta quer fazer digitando o nï¿½mero referente:
+echo Para iniciar precisamos definir qual vai ser nosso c lculo!
+echo Decida que tipo de conta quer fazer digitando o n£mero referente:
 echo 1 - SOMA
-echo 2 - SUBTRAï¿½ï¿½O
-echo 3 - MULTIPLICAï¿½ï¿½O
-echo 4 - DIVISï¿½O
+echo 2 - SUBTRA€ÇO
+echo 3 - MULTIPLICA€ÇO
+echo 4 - DIVISÇO
 echo 5 - SAIR SEM FAZER CONTA
 
 echo.
-:: Definiï¿½ï¿½o do operador
-choice /c "12345" /n /m "Digite sua opï¿½ï¿½o>"
+:: Defini‡Æo do operador
+choice /c "12345" /n /m "Digite sua op‡Æo>"
 goto %errorlevel%
 
 cls
 
 :: Calculando a Soma
 :1
-echo ï¿½tima opï¿½ï¿½o, vamos colocar os nï¿½meros que vamos somar.
+echo àtima op‡Æo, vamos colocar os n£meros que vamos somar.
 echo.
-set /p "n1=Digite o primeiro nï¿½mero: "
+set /p "n1=Digite o primeiro n£mero: "
 echo.
-set /p "n2=Massa, agora digite o segundo nï¿½mero para finalizarmos o cï¿½lculo: "
+set /p "n2=Massa, agora digite o segundo n£mero para finalizarmos o c lculo: "
 set /a conta=%n1%+%n2%
 
 echo A soma de %n1% + %n2% = %conta%
 set n1=0
 set n2=0
 set conta=0
-echo Aperte enter para voltar ao inï¿½cio.
+echo Aperte enter para voltar ao in¡cio.
 pause >nul
 goto :inicio
 
-:: Calculando a Subtraï¿½ï¿½o
+:: Calculando a Subtra‡Æo
 :2
-echo Se e pra tirar vamos nessa, coloque os nï¿½meros que vamos subtrair.
+echo Se e pra tirar vamos nessa, coloque os n£meros que vamos subtrair.
 echo.
-set /p "n1=Digite o primeiro nï¿½mero: "
+set /p "n1=Digite o primeiro n£mero: "
 echo.
-set /p "n2=Massa, agora digite o segundo numero para finalizarmos o cï¿½lculo: "
+set /p "n2=Massa, agora digite o segundo numero para finalizarmos o c lculo: "
 set /a conta=%n1%-%n2%
 
 echo A subtracao foi %n1% - %n2% = %conta%
 set n1=0
 set n2=0
 set conta=0
-echo Aperte enter para voltar ao inï¿½cio.
+echo Aperte enter para voltar ao in¡cio.
 pause >nul
 goto :inicio
 
-:: Calculando a Multiplicaï¿½ï¿½o
+:: Calculando a Multiplica‡Æo
 :3
-echo Entendi, hora de multiplicar, coloque os nï¿½meros que vamos utilizar.
+echo Entendi, hora de multiplicar, coloque os n£meros que vamos utilizar.
 echo.
-set /p "n1=Digite o primeiro nï¿½mero: "
+set /p "n1=Digite o primeiro n£mero: "
 echo.
-set /p "n2=Massa, agora digite o segundo nï¿½mero para finalizarmos o cï¿½lculo: "
+set /p "n2=Massa, agora digite o segundo n£mero para finalizarmos o c lculo: "
 set /a conta=%n1%*%n2%
 
 echo A multiplicacao de %n1% X %n2% = %conta%
 set n1=0
 set n2=0
 set conta=0
-echo Aperte enter para voltar ao inï¿½cio.
+echo Aperte enter para voltar ao in¡cio.
 pause >nul
 goto :inicio
 
-:: Calculando a Divisï¿½o
+:: Calculando a DivisÆo
 :4
-echo Entendi, hora de dividir, coloque os nï¿½meros que vamos utilizar.
+echo Entendi, hora de dividir, coloque os n£meros que vamos utilizar.
 echo.
-set /p "n1=Digite o primeiro nï¿½mero: "
+set /p "n1=Digite o primeiro n£mero: "
 echo.
-set /p "n2=Massa, agora digite o segundo nï¿½mero para finalizarmos o cï¿½lculo: "
+set /p "n2=Massa, agora digite o segundo n£mero para finalizarmos o c lculo: "
 set /a conta=%n1%/%n2%
 set /a resto=%n1%%%n2%
 
-echo A divisï¿½o de %n1% por %n2% = %conta% e sobra %resto%
+echo A divisÆo de %n1% por %n2% = %conta% e sobra %resto%
 set n1=0
 set n2=0
 set conta=0
-echo Aperte enter para voltar ao inï¿½cio.
+echo Aperte enter para voltar ao in¡cio.
 pause >nul
 goto :inicio
 
 :5
-msg * Ok, obrigado por vir ate a proxima.
 exit
