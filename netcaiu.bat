5917ca7efbeb034e237b6750bd9547f405eb69a6
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2020-07-29 12:00:56 -0300

    Adicionado looping com falha

--- netcaiu.bat
+++ netcaiu.bat
@@ -1,24 +1,25 @@
-::@echo off
+@echo off
 :: Teste internet
 
 del c:\pingteste\teste.txt
 mkdir c:\pingteste\
 cd "c:\pingteste\"
 
+echo "TESTANDO..."
 ::setlocal enabledelayedexpansion
 :testeping
 set resultado=0
-ping -n 2 "www.google.com.br" > teste.txt
+ping -n 1 "www.google.com.br" > teste.txt
 type teste.txt | find /i "Resposta">sucesso.txt
 
 for /f "tokens=1" %%a in (sucesso.txt) do (set "resultado=%%a")
 
 echo %resultado%
 
-if "%resultado%" == "Resposta" (goto testeping) else (goto caiu)
+if "%resultado%" == "Resposta" (echo "CONEXAO OK" & goto testeping) else (goto caiu)
 :caiu
 echo "INTERNET CAIU, VERIFIQUE SUA CONEXAO!"
-pause
+goto testeping
 
 
 
